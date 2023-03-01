0122a86919b594918437e0f004260861fc1016ad
Canius-p <boiinventer@gmail.com>
2023-03-01 23:10:20 +0545

    Creating Series in Java

---
+++ series.java
@@ -0,0 +1,19 @@
+
+/* JAva Program to print a series */
+/*1*/
+/*1 2 */
+/*1 2 3 */
+/*1 2 3 4 */
+/*1 2 3 4 5 */
+
+public class series {
+public static void main(String args[]){
+  for(int i = 1; i<5;i++){
+    for(int j = 1;j<i+1; j++){
+      System.put.printn(j +" ");
+}
+System.out.printn();
+  }
+}
+}
+
