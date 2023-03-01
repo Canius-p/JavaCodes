954a1ef847b3d654bfb5e0426c20528a6a192fd6
Canius-p <boiinventer@gmail.com>
2023-03-02 01:11:38 +0545

    fibonacci series

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+// Recursive implementation of Fibonacci Series
+
+class Fibonacci{
+
+	// Function to print the fibonacci series
+	static int fib(int n)
+	{
+		if (n <= 1)
+			return n;
+		return fib(n - 1)
+			+ fib(n - 2);
+	}
+
+	  public static void main(String args[])
+	{
+   int c = 10;
+		for (int i = 0; i < c; i++) {
+
+			System.out.print(fib(i) + " ");
+		}
+	}
+}
+
